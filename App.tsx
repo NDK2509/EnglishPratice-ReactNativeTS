25e1f111572064d42b932a2f8175fbe84cc07d8b
Nguyễn Đăng Kỳ <87688740+NDK2509@users.noreply.github.com>
2022-10-09 00:19:53 +0700

    Add: React Navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import TodosScreen from './screens/TodosScreen';
+import {HomeScreen, WordListScreen} from './screens';
 
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <TodosScreen/>
-  )
+    // <TodosScreen/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="WordList" component={WordListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 export default App
